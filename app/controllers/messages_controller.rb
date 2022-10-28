54825085b05ee837fd66120c725125ce4de1d986
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-10-28 17:41:25 +0700

    chat app working without action cable

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Messages Controller
+class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render 'chatrooms/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Chatroom model
 class Chatroom < ApplicationRecord
   has_many :messages
 end
